b67576ff79aba86b383972b4ee60eea7fba1ecfa
My Name <abbdrhm@gmail.com>
2022-12-23 19:05:57 +0300

    first commit try

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String cardNum = sc.nextLine();
+        //String cardNum = sc.nextLine();
         Scanner sc2 = new Scanner(System.in);
-        int pin = sc2.nextInt();
+        //int pin = sc2.nextInt();
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con= DriverManager.getConnection(
                     "jdbc:mysql://localhost:3306/atm","root","root");
         Statement stmt=con.createStatement();
-        ResultSet rs=stmt.executeQuery("SELECT PIN FROM clients WHERE cardNum=CardNum");
-        System.out.println(rs.getInt(4));
-            if(rs.getInt(4) == pin)
-            {
-                System.out.println("pin is correct");
-            }
+        ResultSet rs=stmt.executeQuery("SELECT Name,Surname FROM clients");
+            while(rs.next())
+                System.out.println(rs.getString(1) + " " + rs.getString(2) );
+            con.close();
 
 
         }catch(Exception e){ System.out.println(e);}

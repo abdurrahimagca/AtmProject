299fb1bc6efefd6c425832f489e00fe42969a692
My Name <abbdrhm@gmail.com>
2022-12-24 14:15:01 +0300

    login pre-finish

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,92 @@
-public class Login {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Login extends Card {
+
+
+    public Login(String cardNum, String pin) {
+        super(cardNum, pin);
+    }
+    private static String query;
+    private static String cardsPin;
+    private static String cardsNum;
+    private static int attempts = 0;
+
+    private static boolean isCardValid(String cardNum) throws SQLException
+    {
+
+
+            query = "SELECT Name FROM clients WHERE CardNum=" + cardNum;
+            //System.out.println(query);
+            ResultSet checkRs = SqlQuery.getResult(query);
+
+                if(!checkRs.next()) {
+                    System.out.println("hatali bir kart numarasi girdiniz lutfen tekrar deneyiniz.. ");
+                    return false;
+                }
+
+            return true;
+
+
+    }
+
+    private static boolean isPinTrue(String cardsPin, String cardPin)
+    {
+
+
+        if (attempts > 3)
+            {
+                //todo: cardnum PIN degeri BLOCKED olarak değiştiriip uygulamadan set edilmelidir.
+                //todo: bu bir sql querysidir.
+                return false;
+
+            }
+            else if (cardPin.length() == 4)
+            {
+                try {
+                    int x = Integer.parseInt(cardPin);
+                    query = "SELECT PIN FROM clients WHERE CardNum=" + cardNum;
+                    ResultSet rs = SqlQuery.getResult(query);
+
+                    //burada varolan uyarı anlamsız, not null veri döndürüyor
+                    while (rs.next())
+                        cardsPin = rs.getString("PIN");
+
+                    if (pin.equals(cardsPin))
+                    {
+                        System.out.println("Sifre Dogru");
+                        return true;
+                    }
+                    else
+                    {
+                        System.out.println("sifreniz hatali, lutfen tekrar deneyiniz..");
+                    }
+
+                } catch (Exception e) {
+                    attempts++;
+                    System.out.println("Pin rakamlardan olusmalidir. ");
+                    return false;
+
+                }
+            }
+            else
+            {
+                attempts++;
+                System.out.println("Lutfen Dort Haneli Pininizi Giriniz: ");
+                return false;
+
+            }
+        return false;
+    }
+
+    public static boolean login() throws SQLException {
+        if (isCardValid(cardNum) && isPinTrue(cardNum, pin))
+        {
+            System.out.println("Hosgeldiniz. Basariyla giris yaptiniz..");
+            //todo: sql tabanindan kullanicinin adi cekilip ekrana basilmali
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,15 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        //todo:bura tamamen ayrı bi class olabilecek kadar uygulamadan bağımsız bir main ?
-        //todo: kart numarasının valid olduğundan emin olunmalı,bu database'de oldugundan emin olunmasıyla
-        //todo: aynıdır
-
-        //BU GITHUB DENEME SATIRI
-
-
-
-        System.out.println("Lutfen Kart Numaranizi Giriniz: ");
-        Scanner sc = new Scanner(System.in);
-        String cardNum;
-        String query;
-        String pin;
-        String cardsPin = null;
+    public static void main(String[] args) throws Exception {
         do {
-            cardNum = sc.next();
-            query = "SELECT Name FROM clients WHERE CardNum=" + cardNum;
-            //System.out.println(query);
-            ResultSet checkRs = SqlQuery.getResult(query);
-            if (!checkRs.next())
-                System.out.println("hatali bir kart numarasi girdiniz lutfen tekrar deneyiniz.. ");
-            else
-                break;
-           } while (true);
+            Scanner sc = new Scanner(System.in);
+            String CardNum = sc.next();
+            String Pin = sc.next();
 
+            Card card = new Login(CardNum, Pin);
+        }while(!Login.login());
 
-        System.out.println("Lutfen dort haneden olusan pininizi giriniz: ");
-        int attempts = 0;
-        do {
-            pin = sc.next();
-            if (attempts > 3)
-            {
-                //todo: cardnum PIN degeri BLOCKED olarak değiştiriip uygulamadan set edilmelidir.
-                //todo: bu bir sql querysidir.
-
-            }
-            else if (pin.length() == 4)
-            {
-                try {
-                    int x = Integer.parseInt(pin);
-                    query = "SELECT PIN FROM clients WHERE CardNum=" + cardNum;
-                    ResultSet rs = SqlQuery.getResult(query);
-
-                        //burada varolan uyarı anlamsız, not null veri döndürüyor
-                    while (rs.next())
-                        cardsPin = rs.getString("PIN");
-
-                    if (pin.equals(cardsPin))
-                    {
-                        System.out.println("Sifre Dogru");
-                        break;
-                    }
-                    else
-                    {
-                        System.out.println("sifreniz hatali, lutfen tekrar deneyiniz..");
-                    }
-
-                } catch (Exception e) {
-                    attempts++;
-                    System.out.println("Pin rakamlardan olusmalidir. ");
-                }
-            }
-            else
-            {
-                attempts++;
-                System.out.println("Lutfen Dort Haneli Pininizi Giriniz: ");
-            }
-
-        } while (true);
 
 
     }
